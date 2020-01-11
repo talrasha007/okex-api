5ef58d7342d2072b03f2769248cb61b6735b6763
Tal <talrasha007@gmail.com>
2020-01-11 13:28:18 -0500

    mv trade to folder

--- src/trade/index.js
+++ src/trade/index.js
@@ -1,54 +1,6 @@
 const { EventEmitter } = require('events');
 
-class Order {
-  constructor(orderWatcher, orderRet) {
-    if (!Order._listened.has(orderWatcher)) {
-      Order._listened.add(orderWatcher);
-      orderWatcher.on('order', Order.processOrder);
-    }
-
-    const { result, error_code, order_id } = orderRet;
-
-    if (!result || error_code * 1 !== 0) {
-      this._error = { result, error_code };
-    } else {
-      this._finishPromise = new Promise(resolve => Order._watched.set(order_id, order => {
-        this.data = order;
-        if (Order.isFinished(order)) resolve();
-      }));
-    }
-  }
-
-  isFinished() {
-    return this.data && Order.isFinished(this.data);
-  }
-
-  async waitForFinish() {
-    if (this._error) {
-      throw new Error(JSON.stringify(this._error));
-    } else {
-      if (!this.isFinished()) await this._finishPromise;
-    }
-  }
-
-  static _listened = new Set();
-  static _watched = new Map();
-
-  static processOrder(order) {
-    const { order_id } = order;
-
-    const cb = Order._watched.get(order_id);
-    if (cb) {
-      if (Order.isFinished(order)) Order._watched.delete(order_id);
-      cb(order);
-    }
-  }
-
-  static isFinished(order) {
-    const { state } = order;
-    return state * 1 === 2 || state < 0;
-  }
-}
+const Order = require('./order');
 
 class Trade extends EventEmitter {
   constructor(wsApi, httpApi) {
