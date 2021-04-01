b396c7d0d4868419e2981a58820ea6fe932527e9
Tal <talrasha007@gmail.com>
2021-04-01 08:49:48 +0800

    v3 example support base url

--- examples/v3/api.js
+++ examples/v3/api.js
@@ -1,7 +1,9 @@
 const { HttpApi, WsApi } = require('../../').v3;
 const http2 = require('http2-wrapper');
 
-const httpApi = new HttpApi(process.env.API_KEY, process.env.API_SECRET, process.env.API_PASS, { transport: http2 });
+const opt = process.env.BASE_URL ? { baseURL: process.env.BASE_URL } : {};
+
+const httpApi = new HttpApi(process.env.API_KEY, process.env.API_SECRET, process.env.API_PASS, { ...opt, transport: http2 });
 const wsApi = new WsApi(process.env.API_KEY, process.env.API_SECRET, process.env.API_PASS, { httpApi });
 
 module.exports = { httpApi, wsApi };
