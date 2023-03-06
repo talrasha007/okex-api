38d93b27002e783f006f640c6c5b2df715606bed
Tal <talrasha007@gmail.com>
2023-03-06 20:08:43 +0800

    0.13.2 use www.okx.com & ws.okx.com.

--- src/v5/http-api.js
+++ src/v5/http-api.js
@@ -5,7 +5,7 @@ const Signer = require('../signer');
 class HttpApi {
   constructor(apiKey, apiSecret, passphrase, opt = {}) {
     if (arguments.length === 1) opt = apiKey;
-    opt.baseURL = opt.baseURL || 'https://aws.okx.com';
+    opt.baseURL = opt.baseURL || 'https://www.okx.com';
 
     this.update(apiKey, apiSecret, passphrase);
     this._http = axios.create(opt);
--- src/v5/ws-api.js
+++ src/v5/ws-api.js
@@ -24,13 +24,13 @@ class WsApi extends EventEmitter {
       }
     };
 
-    this._public = new WS('wss://wsaws.okx.com:8443/ws/v5/public');
+    this._public = new WS('wss://ws.okx.com:8443/ws/v5/public');
     this._public.on('error', console.error);
     this._public.on('message', processMsg);
 
     if (apiSecret) {
       this.update(apiKey, apiSecret, passphrase);
-      this._private = new WS('wss://wsaws.okx.com:8443/ws/v5/private');
+      this._private = new WS('wss://ws.okx.com:8443/ws/v5/private');
       this._private.on('error', console.error);
       this._private.on('open', () => {
         const [timestamp, sign] = this.signer.sign('/users/self/verify');
