2609ee5796fe3f772a271c41d82286f996b09d24
Tal <talrasha007@gmail.com>
2025-03-14 19:05:55 +0800

    WsApi init.

--- src/WsApi.ts
+++ src/WsApi.ts
@@ -1,4 +1,10 @@
-
+import { APICredentials } from './utils';
 
 export class WsApi {
+  public static async create(apiKey: string, apiSecret: string, passphrase: string, baseURL = 'wss://ws.okx.com:8443') {
+    return new WsApi(await APICredentials.create(apiKey, apiSecret, passphrase));
+  }
+
+  constructor(private credentials?: APICredentials, private baseURL = 'wss://ws.okx.com:8443') { }
+
 }
