a88b693eda04d9a3d975549b2be25a94a7e8607e
talrasha007 <talrasha007@gmail.om>
2025-03-15 09:56:04 +0800

    ws api base.

--- src/WsApi.ts
+++ src/WsApi.ts
@@ -1,10 +1,46 @@
 import { APICredentials } from './utils';
 
-export class WsApi {
-  public static async create(apiKey: string, apiSecret: string, passphrase: string, baseURL = 'wss://ws.okx.com:8443') {
-    return new WsApi(await APICredentials.create(apiKey, apiSecret, passphrase));
+class WsApi extends EventTarget {
+  private ws?: WebSocket;
+
+  constructor(private url: string) {
+    super();
   }
 
-  constructor(private credentials?: APICredentials, private baseURL = 'wss://ws.okx.com:8443') { }
+  connect() {
+    const ws = new WebSocket(this.url);
 
+    ws.onmessage = (event) => {
+      this.dispatchEvent(new MessageEvent('message', { data: event.data }));
+    };
+
+    ws.onopen = (event) => {
+      this.ws = ws;
+      this.dispatchEvent(new Event('open'));
+    };
+
+    ws.onclose = (event) => {
+      this.dispatchEvent(new CloseEvent('close', { code: event.code, reason: event.reason }));
+      this.ws = undefined;
+    };
+
+    ws.onerror = (event) => {
+      this.dispatchEvent(new ErrorEvent('error', { error: event.error }));
+      ws.close();
+    };
+  }
+}
+
+export class WsPrivate extends WsApi {
+  public static async create(apiKey: string, apiSecret: string, passphrase: string, baseURL = 'wss://ws.okx.com:8443') {
+    const credentials = await APICredentials.create(apiKey, apiSecret, passphrase);
+    if (credentials)
+      return new WsPrivate(credentials!, baseURL);
+    else
+      throw new Error('No credentials');
+  }
+
+  constructor(credentials: APICredentials, baseURL = 'wss://ws.okx.com:8443') {
+    super(baseURL + '/ws/v5/private');
+  }
 }
