46934dc11506baaabce92f5f132fc17346f57dc3
Tal <talrasha007@gmail.com>
2025-03-15 17:06:12 +0800

    WsTrade types.

--- src/types/websockets/event.ts
+++ src/types/websockets/event.ts
@@ -1,4 +1,4 @@
-import type { WsChannel } from './request';
+import type { WsChannel, WsTradeOp } from './request';
 
 export interface WsEvent {
   event: 'error' | 'login' | 'subscribe' | 'unsubscribe' | 'channel-conn-count';
@@ -48,4 +48,52 @@ export interface WsTicker {
   volCcy24h: string;
   vol24h: string;
   ts: string;
+}
+
+export interface WsTradeRequest<T> {
+  id: string;
+  op: WsTradeOp;
+  args: T[];
+}
+
+export interface WsOrderArg {
+  instId: string;
+  tdMode?: 'isolated' | 'cross' | 'cash' | 'spot_isolated';
+  ccy?: string;
+  clOrdId?: string;
+  tag?: string;
+  side: 'buy' | 'sell';
+  posSide?: 'net' | 'long' |'short';
+  ordType: 'market' | 'limit' | 'post_only' | 'ioc' | 'fok' | 'optimal_limit_ioc' | 'mmp' | 'mmp_and_post_only';
+  px: string;
+  sz: string;
+  reduceOnly?: boolean;
+  tgtCcy?: string;
+  stpMode?: 'cancel_maker' | 'cancel_taker' | 'cancel_both';
+}
+
+export interface WsCancelOrderArg {
+  instId: string;
+  ordId: string;
+  clOrdId?: string;
+}
+
+export interface WsTradeOrderResult {
+  clOrdId: string;
+  ordId: string;
+  ts: string;
+  sCode: string;
+  sMsg: string;
+  reqId?: string;
+  tag?: string;
+}
+
+export interface WsTradeOpEvent {
+  id: string;
+  op: WsTradeOp;
+  data: WsTradeOrderResult[];
+  code: string;
+  msg: string;
+  inTime: string;
+  outTime: string;
 }
