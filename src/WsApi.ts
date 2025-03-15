b9d4768959e483335dbb3d47e4e5a188cf094a3b
Tal <talrasha007@gmail.com>
2025-03-15 11:23:23 +0800

    ping & wait for ready.

--- src/WsApi.ts
+++ src/WsApi.ts
@@ -9,19 +9,22 @@ class WsApi extends EventTarget {
   }
 
   connect() {
+    let pingInterval: Timer;
     const ws = new WebSocket(this.url);
 
     ws.onmessage = (event) => {
       this.dispatchEvent(new MessageEvent('message', { data: event.data }));
     };
 
-    ws.onopen = (event) => {
+    ws.onopen = () => {
       this.ws = ws;
       this.dispatchEvent(new Event('open'));
+      pingInterval = setInterval(() => ws.send('ping'), 10 * 1000);
     };
 
     ws.onclose = (event) => {
       this.dispatchEvent(new CloseEvent('close', { code: event.code, reason: event.reason }));
+      clearInterval(pingInterval);
       if (this.ws === ws) {
         this.ws = undefined;
         if (this.shouldReconnect) {
@@ -40,6 +43,29 @@ class WsApi extends EventTarget {
     this.shouldReconnect = false;
     if (this.ws)
       this.ws.close();
+  }
+
+  async waitForReady(timeout = 10 * 1000) {
+    if (!this.ws) {
+      return new Promise((resolve, reject) => {
+        const controller  = new AbortController();
+        const timeoutId = setTimeout(() => {
+          controller.abort();
+          reject(new Error('Timeout'));
+        }, timeout);
+
+        this.addEventListener('open', () => {
+          resolve(undefined);
+          clearTimeout(timeoutId);
+        }, { once: true, signal: controller.signal });
+      });
+    }
+  }
+
+  private async send(data: any) {
+    if (typeof data !== 'string') data = JSON.stringify(data);
+    await this.waitForReady();
+    this.ws!.send(data);    
   }
 }
 
